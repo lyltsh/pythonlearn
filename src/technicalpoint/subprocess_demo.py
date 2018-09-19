900328c24323933760cefb6a775c915a2ee3d920
leiyulin <leiyulin@kingsoft.com>
2018-09-19 20:20:05 +0800

    subprocess commit

---
+++ src/technicalpoint/subprocess_demo.py
@@ -0,0 +1,14 @@
+import subprocess
+
+"""
+subprocess模块用来创建新的进程，连接到其stdin、stdout、stderr管道并获取它们的返回码。
+subprocess模块的出现是为了替代如下旧模块及函数：os.system、os.spawn*、os.popen*、popen2.*、commands.*
+"""
+# subprocess demo
+
+res = subprocess.run("ls")
+print(res)
+
+print(subprocess.run("ls -ltr", shell=True))
+
+print(subprocess.run(["ls", "-l"]))
