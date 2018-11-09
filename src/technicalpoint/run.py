28b2c1a4741c3dd23545505f0eb2e8badb89d4f3
leiyulin <leiyulin@kingsoft.com>
2018-11-09 18:08:05 +0800

    subprocess

---
+++ src/technicalpoint/run.py
@@ -0,0 +1,5 @@
+from pip._vendor.distlib.compat import raw_input
+
+i = int(raw_input("please input a numner:"))
+
+print("you input a number: ", i)
--- src/technicalpoint/subprocess_demo.py
+++ src/technicalpoint/subprocess_demo.py
@@ -13,5 +13,37 @@ print(subprocess.run("ls -ltr", shell=True))
 
 print(subprocess.run(["ls", "-l"]))
 
-
 print(subprocess.run("echo hello", shell=True))
+
+# run another python file
+"""
+注意不要为stdout和stderr参数赋值subprocess.PIPE，如果子进程输出量较多会造成死锁，
+这两个参数可以赋值为subprocess.STDOUT打印到屏幕或者赋值为一个文件对象将输出写入文件：
+"""
+# subprocess.call("python3 run.py", shell=True)
+subprocess.call("python3 run.py", shell=True, stdin=open('fake_input', 'r'), stdout=open('result', 'w'))
+
+# check_call
+try:
+    res = subprocess.check_call(["ls", "("])
+    print("res:", res)
+except subprocess.CalledProcessError as exc:
+    print("return_code", exc.returncode)
+    print("cmd", exc.cmd)
+    print("output:", exc.output)
+
+# check_output
+try:
+    res = subprocess.check_output(["ls", "sss"], shell=True, stderr=subprocess.STDOUT)
+    print(res)
+except subprocess.CalledProcessError as exc:
+    print("return_code", exc.returncode)
+    print("cmd", exc.cmd)
+    print("output:", exc.output)
+
+filename = "fake_input"
+# macOS: md5  linux: md5sum
+res = subprocess.check_output(["md5 {}".format(filename)], shell=True)
+print(res)
+
+print(-4 % 3)
